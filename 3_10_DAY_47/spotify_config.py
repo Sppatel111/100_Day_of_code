8820096f0dba36a2015f68a5fe6843b858619a27
SPatel11111111 <sneha.patel1@aliansoftware.com>
2025-03-10 11:57:18 +0530

    Revert "virtual environment setup"
    This reverts commit 744859d4afbe887c451585dd3473ac6b39ea5aac.

--- 3_10_DAY_47/spotify_config.py
+++
@@ -1,7 +0,0 @@
-#developers.spotify.com
-#app name:Billboard to Spotify
-ClientID='d196310cdbb24e8692e2c1ad69f90b23'
-Clientsecret='578c2b8f255042828b40d4e23d0eba2a'
-USERNAME='sneha'
-# example.com
-# http://127.0.0.1:300
